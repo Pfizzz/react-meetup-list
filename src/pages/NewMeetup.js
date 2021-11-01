877ede4aa7e9e53e84f8e915f8cb3868d9c000c2
Pfizz <pfiste80@gmail.com>
2021-11-01 19:13:29 -0400

    fetch request is functional

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,25 @@
+import { useHistory } from 'react-router-dom';
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 function NewMeetupPage() {
+    const history = useHistory();
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-getting-started-8326f-default-rtdb.firebaseio.com/meetups.json',
+        {
+            method: 'POST', 
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }
+        ).then(() => {
+            history.replace('/');
+        })
+    }
     return <section>
         <h1>Add new meetup</h1>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>
 }
 
